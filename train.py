48b5b567b0ac1e15a3f0fdef64652b5b5c90b654
anurag baundwal <52976197+Anurag-Baundwal@users.noreply.github.com>
2025-02-05 15:12:16 +0530

    updates10

--- train.py
+++ train.py
@@ -36,6 +36,7 @@ def generate_games_parallel(simulations_per_move, temp_threshold, num_processes,
     print(f"Generating {total_games} games across {processes} processes...")
     with multiprocessing.Pool(processes=processes) as pool:
         game_args = [(simulations_per_move, temp_threshold) for _ in range(total_games)]
+        print(f"game_args: {game_args}")  # ADD THIS LINE
         games_data_list = pool.starmap(_generate_game_static, game_args)
     return [item for sublist in games_data_list for item in sublist]
 
